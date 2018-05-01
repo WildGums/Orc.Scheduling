a1859de2a7a003cd3321dd9c3ce7b9691ac8bf7a
Geert van Horrik <geert@catenalogic.com>
2018-05-01 12:16:14 +0200

    Migrate test project to new project structure

--- src/Orc.Scheduling.Tests/PublicApiFacts.cs
+++ src/Orc.Scheduling.Tests/PublicApiFacts.cs
@@ -7,13 +7,14 @@
 
 namespace Orc.Scheduling.Tests
 {
+    using System.Runtime.CompilerServices;
     using ApiApprover;
     using NUnit.Framework;
 
     [TestFixture]
     public class PublicApiFacts
     {
-        [Test]
+        [Test, MethodImpl(MethodImplOptions.NoInlining)]
         public void Orc_Scheduling_HasNoBreakingChanges()
         {
             var assembly = typeof(SchedulingService).Assembly;
