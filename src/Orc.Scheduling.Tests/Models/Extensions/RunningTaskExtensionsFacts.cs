c661719a0e0fd076c1576afa12c3a13ed6f4d908
Geert van Horrik <geert@catenalogic.com>
2023-01-12 19:40:08 +0100

    Fix analyzers

--- src/Orc.Scheduling.Tests/Models/Extensions/RunningTaskExtensionsFacts.cs
+++ src/Orc.Scheduling.Tests/Models/Extensions/RunningTaskExtensionsFacts.cs
@@ -1,11 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="RunningTaskExtensionsFacts.cs" company="WildGums">
-//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-
-namespace Orc.Scheduling.Tests.Models
+﻿namespace Orc.Scheduling.Tests.Models
 {
     using System;
     using System.Threading.Tasks;
--- src/Orc.Scheduling.Tests/Services/SchedulingServiceFacts.cs
+++ src/Orc.Scheduling.Tests/Services/SchedulingServiceFacts.cs
@@ -1,11 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="SchedulingServiceFacts.cs" company="WildGums">
-//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-
-namespace Orc.Scheduling.Tests.Services
+﻿namespace Orc.Scheduling.Tests.Services
 {
     using System;
     using System.Linq;
