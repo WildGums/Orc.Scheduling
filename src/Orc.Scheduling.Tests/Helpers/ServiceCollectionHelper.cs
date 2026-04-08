a974ba6f3aa58b2bf64fc9b59fbd6b23cccf7f76
Geert van Horrik <geert@catenalogic.com>
2026-04-08 09:27:07 +0200

    Convert to file scoped namespaces

--- src/Orc.Scheduling.Tests/Helpers/ServiceCollectionHelper.cs
+++ src/Orc.Scheduling.Tests/Helpers/ServiceCollectionHelper.cs
@@ -1,20 +1,19 @@
-﻿namespace Orc.Scheduling.Tests
+﻿namespace Orc.Scheduling.Tests;
+
+using Catel;
+using Microsoft.Extensions.DependencyInjection;
+using Orc.Scheduling;
+
+internal static class ServiceCollectionHelper
 {
-    using Catel;
-    using Microsoft.Extensions.DependencyInjection;
-    using Orc.Scheduling;
-
-    internal static class ServiceCollectionHelper
+    public static IServiceCollection CreateServiceCollection()
     {
-        public static IServiceCollection CreateServiceCollection()
-        {
-            var serviceCollection = new ServiceCollection();
+        var serviceCollection = new ServiceCollection();
 
-            serviceCollection.AddLogging();
-            serviceCollection.AddCatelCore();
-            serviceCollection.AddOrcScheduling();
+        serviceCollection.AddLogging();
+        serviceCollection.AddCatelCore();
+        serviceCollection.AddOrcScheduling();
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
--- src/Orc.Scheduling/OrcSchedulingModule.cs
+++ src/Orc.Scheduling/OrcSchedulingModule.cs
@@ -1,26 +1,25 @@
-﻿namespace Orc
+﻿namespace Orc;
+
+using Catel.Services;
+using Catel.ThirdPartyNotices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Orc.Scheduling;
+
+/// <summary>
+/// Core module which allows the registration of default services in the service collection.
+/// </summary>
+public static class OrcSchedulingModule
 {
-    using Catel.Services;
-    using Catel.ThirdPartyNotices;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.DependencyInjection.Extensions;
-    using Orc.Scheduling;
-
-    /// <summary>
-    /// Core module which allows the registration of default services in the service collection.
-    /// </summary>
-    public static class OrcSchedulingModule
+    public static IServiceCollection AddOrcScheduling(this IServiceCollection serviceCollection)
     {
-        public static IServiceCollection AddOrcScheduling(this IServiceCollection serviceCollection)
-        {
-            serviceCollection.TryAddSingleton<ITimeService, TimeService>();
-            serviceCollection.TryAddSingleton<ISchedulingService, SchedulingService>();
+        serviceCollection.TryAddSingleton<ITimeService, TimeService>();
+        serviceCollection.TryAddSingleton<ISchedulingService, SchedulingService>();
 
-            serviceCollection.AddSingleton<ILanguageSource>(new LanguageResourceSource("Orc.Scheduling", "Orc.Scheduling.Properties", "Resources"));
+        serviceCollection.AddSingleton<ILanguageSource>(new LanguageResourceSource("Orc.Scheduling", "Orc.Scheduling.Properties", "Resources"));
 
-            serviceCollection.AddSingleton<IThirdPartyNotice>((x) => new LibraryThirdPartyNotice("Orc.Scheduling", "https://github.com/wildgums/orc.scheduling"));
+        serviceCollection.AddSingleton<IThirdPartyNotice>((x) => new LibraryThirdPartyNotice("Orc.Scheduling", "https://github.com/wildgums/orc.scheduling"));
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
